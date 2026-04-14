75acd91fb73bcc617f47cae6603f8ccc32bc01ca
MisterJokestar <james@marzhillstudios.com>
2026-04-14 11:12:22 -0400

    scaffold for frontend

---
+++ frontend/pages/+Layout.tsx
@@ -0,0 +1,8 @@
+import "./Layout.css";
+
+import logoUrl from "../assets/logo.svg";
+import { Link } from "../components/Link";
+
+export default function Layout({ children }: { children: React.ReactNode }) {
+  return <div className="">{children}</div>;
+}
---
+++ frontend/vite.config.ts
@@ -0,0 +1,9 @@
+import react from "@vitejs/plugin-react";
+/// <reference types="@batijs/core/types" />
+import tailwindcss from "@tailwindcss/vite";
+import vike from "vike/plugin";
+import { defineConfig } from "vite";
+
+export default defineConfig({
+  plugins: [vike(), react(), tailwindcss()],
+});
