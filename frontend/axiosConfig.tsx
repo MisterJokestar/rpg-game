558a8177518da43143e7ad94cbd3787897db6b7f
James Wall <james@marzhillstudios.com>
2026-04-20 12:47:10 -0400

    axios set up

--- frontend/axiosConfig.tsx
+++ frontend/axiosConfig.tsx
@@ -1,7 +1,21 @@
 import axios from "axios";
 
-const apiClient = axios.create({
+// Main server api
+export const apiClient = axios.create({
   baseURL: import.meta.env.VITE_SERVER_BASE_URL ?? "http://localhost:5000",
 });
 
-export default apiClient;
+// Appends the authorization to the reuest headers.
+apiClient.interceptors.request.use((config) => {
+    const auth = localStorage.getItem("auth");
+    if (auth) config.headers.Authorization = auth;
+    return config;
+});
+
+// Sepereate cloud function authorization system.
+export const authClient = axios.create({
+  baseURL: import.meta.env.VITE_AUTH_BASE_URL ?? "http://localhost:5000",
+});
+
+// HOW TO USE
+// const response = await apiClient.get("/games", {request_body});
