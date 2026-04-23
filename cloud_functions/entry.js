1fe5d89d77a5c5774496c0115448a9835797d9e8
Sample Program <shp.plemmons@gmail.com>
2026-04-23 00:14:16 -0400

    updated cloud function for game stats page 📃

--- cloud_functions/entry.js
+++ cloud_functions/entry.js
@@ -17,6 +17,7 @@ const updateCharacterRoute = require('./routes/update_character');
 const createGameRoute = require('./routes/create_game');
 const updateGameRoute = require('./routes/update_game');
 
+const getStatsRoute = require('./routes/get_stats');
 const getLeaderboardRoute = require('./routes/get_leaderboard');
 
 const app = express();
@@ -33,6 +34,7 @@ app.use('/updateCharacter', updateCharacterRoute);
 app.use('/createGame', createGameRoute);
 app.use('/updateGame', updateGameRoute);
 
+app.use('/getStats', getStatsRoute);
 app.use('/getLeaderboard', getLeaderboardRoute);
 
 exports.entryPoint = functions.https.onRequest(app);
